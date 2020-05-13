757b516c791123697896c24be34b5e1b1284bc2f
Rajat Mangal <rmangal@sfu.ca>
2020-05-12 20:34:10 -0700

    Individual Project Component added with some minor adjusts

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,30 +1,15 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
             <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p> Posted By Rajat Mangal</p>
-                    <p className="grey-text">1st January, 2020</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p> Posted By Rajat Mangal</p>
-                    <p className="grey-text">1st January, 2020</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                    </span>
-                    <p> Posted By Rajat Mangal</p>
-                    <p className="grey-text">1st January, 2020</p>
-                </div>
+                <ProjectSummary />
+
+                <ProjectSummary />
+
+                <ProjectSummary />
             </div>
         </div>
     )
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card-content grey-text text-darken-3">
+            <span className="card-title">
+                Project Title
+            </span>
+            <p> Posted By Rajat Mangal</p>
+            <p className="grey-text">1st January, 2020</p>
+        </div>
+    )
+}
+
+export default ProjectSummary
